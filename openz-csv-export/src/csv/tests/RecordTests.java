dfbbd942bc0e654ca4d23d07ab8ace479a3d7101
Christian Gläser <glaeser@7says.de>
2014-10-27 16:20:48 +0100

    Package structure, test

---
+++ openz-csv-export/src/csv/tests/RecordTests.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package csv.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Christian Gläser
+ *
+ */
+public class RecordTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
