b5255b7159413158efb24a702feda3f01ff9f25f
Christian Gläser <glaeser@7says.de>
2014-10-27 16:55:00 +0100

    File renamed from RecordsToCSV to RecordHandler

---
+++ openz-csv-export/src/csv/RecordHandler.java
@@ -0,0 +1,43 @@
+package csv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Christian Gläser
+ * 
+ * Sucht Belege aus openz, erstellt Records (Belege) und speichert diese in einer csv-Datei ab, die über eine http-Schnittstelle ausgegeben wird.
+ *
+ */
+public class RecordHandler {
+	/**
+	 * Der Name der CSV-Datei, sollte in Config-Datei festgelegt werden und dann beim Starten von OpenZ in
+	 */
+	private String fileName;
+	private List<Record> records;
+	
+	/**
+	 * RecordHandler - Dateirückgabe: records.csv 
+	 */
+	public RecordHandler(){
+		this("records.csv");
+	}
+	
+	/**
+	 * @param filename Der Name der zurückzugebenden Datei
+	 */
+	public RecordHandler(String filename){
+		records = new ArrayList<Record>();
+		//Read records;
+		setFileName(filename);
+		
+	}
+
+	public String getFileName() {
+		return this.fileName;
+	}
+
+	public void setFileName(String fileName) {
+		fileName = this.fileName;
+	} 
+}
