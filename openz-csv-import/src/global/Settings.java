16bf540b73adea90bfb41b0a8513074bf04bd978
cglaeser <glaeser@7says.de>
2014-11-11 22:29:25 +0100

    Converted Import to Maven project, added System tray to enable background update

--- openz-csv-import/src/global/Settings.java
+++ openz-csv-import/src/global/Settings.java
@@ -1,9 +1,14 @@
-package io;
+package global;
 
+/**
+ * Globale Einstellungen für die Programinitalisierung
+ * @author HAWAI
+ */
 public class Settings {
 	
 	private String storageLocation;
 	private String folderLocation;
+	private String updateInterval;
 	
 	public String getFolderLocation() {
 		return folderLocation;
@@ -16,6 +21,12 @@ public class Settings {
 	}
 	public void setStorageLocation(String storageLocation) {
 		this.storageLocation = storageLocation;
+	}
+	public String getUpdateInterval() {
+		return updateInterval;
+	}
+	public void setUpdateInterval(String updateInterval) {
+		this.updateInterval = updateInterval;
 	}
 
 }
